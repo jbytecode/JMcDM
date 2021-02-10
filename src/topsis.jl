d4f31585a3554204e62c90667c4afc40cab1034f
jbytecode <mhsatman@gmail.com>
2021-02-10 23:18:58 +0300

    add docs to topsis

--- src/topsis.jl
+++ src/topsis.jl
@@ -1,4 +1,62 @@
-function topsis(decisionMat::DataFrame, weights::Array{Float64,1})::TopsisResult
+"""
+        topsis(decisionMat, weights)
+
+Apply TOPSIS (Technique for Order of Preference by Similarity to Ideal Solution) method 
+for a given matrix and weights.
+
+# Arguments:
+ - `decisionMat::DataFrame`: n × m matrix of objective values for n candidate (or strategy) and m criteria 
+ - `weights::Array{Float64, 1}`: m-vector of weights that sum up to 1.0. If the sum of weights is not 1.0, it is automatically normalized.
+
+# Description 
+topsis() applies the TOPSIS method to rank n strategies subject to m criteria which are supposed to be maximized.
+
+# Output 
+- `::TopsisResult`: TopsisResult object that holds multiple outputs including scores and best index.
+
+# Examples
+```julia-repl
+julia> df = DataFrame();
+julia> df[:, :x] = Float64[9, 8, 7];
+julia> df[:, :y] = Float64[7, 7, 8];
+julia> df[:, :z] = Float64[6, 9, 6];
+julia> df[:, :q] = Float64[7, 6, 6];
+
+julia> w = Float64[4, 2, 6, 8];
+
+julia> df
+3×4 DataFrame
+ Row │ x        y        z        q       
+     │ Float64  Float64  Float64  Float64 
+─────┼────────────────────────────────────
+   1 │     9.0      7.0      6.0      7.0
+   2 │     8.0      7.0      9.0      6.0
+   3 │     7.0      8.0      6.0      6.0
+
+
+julia> result = topsis(df, w);
+
+julia> result.bestIndex
+2
+
+julia> result.scores
+3-element Array{Float64,1}:
+ 0.38768695492211824
+ 0.6503238218850163
+ 0.08347670030339041
+```
+
+# References
+Hwang, C.L.; Yoon, K. (1981). Multiple Attribute Decision Making: Methods and Applications. New York: Springer-Verlag
+
+Celikbilek Yakup, Cok Kriterli Karar Verme Yontemleri, Aciklamali ve Karsilastirmali
+Saglik Bilimleri Uygulamalari ile. Editor: Muhlis Ozdemir, Nobel Kitabevi, Ankara, 2018
+
+İşletmeciler, Mühendisler ve Yöneticiler için Operasyonel, Yönetsel ve Stratejik Problemlerin
+Çözümünde Çok Kriterli Karar verme Yöntemleri, Editörler: Bahadır Fatih Yıldırım ve Emrah Önder,
+Dora, 2. Basım, 2015, ISBN: 978-605-9929-44-8
+"""
+    function topsis(decisionMat::DataFrame, weights::Array{Float64,1})::TopsisResult
     
     w = unitize(weights)
     nalternatives, ncriteria = size(decisionMat)
