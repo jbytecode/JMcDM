65f51c3e4a28c340373f62463a1fa3e9ff80d9aa
bahadirfyildirim@gmail.com <bahadirfyildirim@gmail.com>
2021-02-14 04:41:17 -0800

    add EDAS method

--- src/.dev/test.jl
+++ src/.dev/test.jl
@@ -757,3 +757,28 @@ end
     @test isapprox(result.scores, [0.805021, 0.775060, 0.770181, 0.796424, 0.788239], atol=tol)
     
 end
+
+@testset "EDAS" begin
+    tol = 0.0001
+    decmat = [5000 5 5300 450;
+    4500 5 5000 400;
+    4500 4 4700 400;
+    4000 4 4200 400;
+    5000 4 7100 500;
+    5000 5 5400 450;
+    5500 5 6200 500;
+    5000 4 5800 450]
+
+    df = makeDecisionMatrix(decmat)
+
+    weights = [0.25, 0.25, 0.25, 0.25];
+
+    fns = [maximum, maximum, minimum, minimum];
+
+    result = edas(df, weights, fns)
+
+    @test result isa EDASResult
+
+    @test isapprox(result.scores, [0.759594, 0.886016, 0.697472, 0.739658, 0.059083, 0.731833, 0.641691, 0.385194], atol=tol)
+    
+end
