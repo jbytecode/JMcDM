b5713b9a6068bcade4d801561f578ebcb89da0f2
jbytecode <mhsatman@gmail.com>
2021-02-22 10:24:51 +0300

    add documentation for electre

--- src/electre.jl
+++ src/electre.jl
@@ -1,3 +1,66 @@
+"""
+        electre(decisionMat, weights, fns)
+
+Apply ELECTRE (ELimination Et Choice Translating REality) method 
+for a given matrix and weights.
+
+# Arguments:
+ - `decisionMat::DataFrame`: n × m matrix of objective values for n candidate (or strategy) and m criteria 
+ - `weights::Array{Float64, 1}`: m-vector of weights that sum up to 1.0. If the sum of weights is not 1.0, it is automatically normalized.
+ - `fns::Array{Function, 1}`: m-vector of function that are either minimize or maximize.
+
+# Description 
+electre() applies the ELECTRE method to rank n strategies subject to m criteria which are supposed to be either maximized or minimized.
+
+# Output 
+- `::ElectreResult`: TopsisResult object that holds multiple outputs including scores and best index.
+
+# Examples
+```julia-repl
+julia> w =  [0.110, 0.035, 0.379, 0.384, 0.002, 0.002, 0.010, 0.077];
+julia> Amat = [
+      100 92 10 2 80 70 95 80 ;
+      80  70 8  4 100 80 80 90 ;
+      90 85 5 0 75 95 70 70 ; 
+      70 88 20 18 60 90 95 85
+    ];
+
+julia> dmat = makeDecisionMatrix(Amat);
+julia> fns = makeminmax([maximum for i in 1:8]);
+julia> result = electre(dmat, w, fns)
+
+julia> dmat
+4×8 DataFrame
+ Row │ Crt1     Crt2     Crt3     Crt4     Crt5     Crt6     Crt7     Crt8    
+     │ Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64 
+─────┼────────────────────────────────────────────────────────────────────────
+   1 │   100.0     92.0     10.0      2.0     80.0     70.0     95.0     80.0
+   2 │    80.0     70.0      8.0      4.0    100.0     80.0     80.0     90.0
+   3 │    90.0     85.0      5.0      0.0     75.0     95.0     70.0     70.0
+   4 │    70.0     88.0     20.0     18.0     60.0     90.0     95.0     85.0
+
+julia> result.bestIndex
+(4,)
+
+julia> result.C
+4-element Array{Float64,1}:
+  0.3693693693693696
+  0.01501501501501501
+ -2.473473473473473
+  2.0890890890890894
+
+julia> result.D
+4-element Array{Float64,1}:
+  0.1914244325928971
+ -0.19039293350192432
+  2.884307608766315
+ -2.885339107857288
+```
+
+# References
+Celikbilek Yakup, Cok Kriterli Karar Verme Yontemleri, Aciklamali ve Karsilastirmali
+Saglik Bilimleri Uygulamalari ile. Editor: Muhlis Ozdemir, Nobel Kitabevi, Ankara, 2018
+"""
 function electre(decisionMat::DataFrame, weights::Array{Float64,1}, fns::Array{Function,1})::ElectreResult
 
     w = unitize(weights)
@@ -24,7 +87,7 @@ function electre(decisionMat::DataFrame, weights::Array{Float64,1}, fns::Array{F
                         end
                     else
                         if weightednormalizedMat[i, h] <= weightednormalizedMat[j, h]
-                            push!(betterlist, h)    
+                push!(betterlist, h)    
                         else
                             push!(worselist, h)
                         end
@@ -56,7 +119,7 @@ function electre(decisionMat::DataFrame, weights::Array{Float64,1}, fns::Array{F
         i = elements[:i]
         j = elements[:j]
         elemlist = elements[:set]
-    
+        
         r_ik       = weightednormalizedMat[i, elemlist]
         r_jk       = weightednormalizedMat[j, elemlist]
         r_ik_full  = weightednormalizedMat[i, :]
@@ -87,7 +150,7 @@ function electre(decisionMat::DataFrame, weights::Array{Float64,1}, fns::Array{F
     result = ElectreResult(
         decisionMat,
         w,
-        weightednormalizedMat,
+    weightednormalizedMat,
         fitnessTable,
         nonfitnessTable,
         fitnessmatrix,
