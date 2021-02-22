360a4b434e406f377d89ede0565255dcbb4877a7
jbytecode <mhsatman@gmail.com>
2021-02-22 13:10:50 +0300

    add documentation for promethee

--- src/promethee.jl
+++ src/promethee.jl
@@ -1,3 +1,7 @@
+"""
+    Linear preference function for PROMETHEE.
+
+"""
 function prometLinear(d::Number, q::Number, p::Number)::Float64
     if (d <= q)
         0
@@ -8,6 +12,10 @@ function prometLinear(d::Number, q::Number, p::Number)::Float64
     end
 end
 
+"""
+    V-shaped preference function for PROMETHEE.
+        
+"""
 function prometVShape(d::Number, q::Any, p::Number)::Float64
     if (d <= 0)
         0
@@ -18,7 +26,11 @@ function prometVShape(d::Number, q::Any, p::Number)::Float64
     end
 end
 
-function prometUsual(d::Number, q::Any, p::Any)::Float64
+"""
+    U-Shaped preference function for PROMETHEE.
+        
+"""
+function prometUShape(d::Number, q::Any, p::Any)::Float64
     if (d == 0)
         0
     else
@@ -26,7 +38,10 @@ function prometUsual(d::Number, q::Any, p::Any)::Float64
     end
 end
 
-
+"""
+    Quasi preference function for PROMETHEE.
+        
+"""
 function prometQuasi(d::Number, q::Number, p::Any)::Float64
     if (d <= q)
         0
@@ -35,6 +50,10 @@ function prometQuasi(d::Number, q::Number, p::Any)::Float64
     end
 end
 
+"""
+    Level preference function for PROMETHEE.
+        
+"""
 function prometLevel(d::Number, q::Number, p::Number)::Float64
     if (d <= q)
         0
@@ -45,6 +64,82 @@ function prometLevel(d::Number, q::Number, p::Number)::Float64
     end
 end
 
+"""
+        promethee(decisionMatrix, prefs, weights, fns, qs, ps)
+
+Apply PROMETHEE (Preference Ranking Organization METHod for Enrichment of Evaluations) method for a given matrix and weights.
+
+# Arguments:
+ - `decisionMatrix::DataFrame`: n × m matrix of objective values for n candidate (or strategy) and m criteria 
+ - `pref::Array{Function, 1}`: m-vector of preference functions that are prometLinear, prometVShape, prometUShape, prometQuasi, or prometLevel.
+ - `weights::Array{Float64, 1}`: m-vector of weights that sum up to 1.0. If the sum of weights is not 1.0, it is automatically normalized.
+ - `fns::Array{Function, 1}`: m-vector of functions that are either maximum or minimum.
+ - `qs::Array{Float64, 1}`: m-vector of q parameters that is used in corresponding preference function.
+ - `ps::Array{Float64, 1}`: m-vector of p parameters that is used in corresponding preference function
+ 
+# Description 
+promethee() applies the PROMETHEE method to rank n strategies subject to m criteria which are supposed to be either maximized or minimized.
+
+# Output 
+- `::Result`: PrometheeResult object that holds multiple outputs including scores and best index.
+
+# Examples
+```julia-repl
+julia> decmat = [42.0 35 43 51; 
+                     89 72 92 85;
+                     14 85 17 40;
+                     57 60 45 80;
+                     48 32 43 40;
+                     71 45 60 85;
+                     69 40 72 55;
+                     64 35 70 60];
+
+julia> df = makeDecisionMatrix(decmat)
+8×4 DataFrame
+ Row │ Crt1     Crt2     Crt3     Crt4    
+     │ Float64  Float64  Float64  Float64 
+─────┼────────────────────────────────────
+   1 │    42.0     35.0     43.0     51.0
+   2 │    89.0     72.0     92.0     85.0
+   3 │    14.0     85.0     17.0     40.0
+   4 │    57.0     60.0     45.0     80.0
+   5 │    48.0     32.0     43.0     40.0
+   6 │    71.0     45.0     60.0     85.0
+   7 │    69.0     40.0     72.0     55.0
+   8 │    64.0     35.0     70.0     60.0
+
+julia> qs = [49, nothing, 45, 30];
+
+julia> ps = [100, 98, 95, 80];
+
+julia> weights = [0.25, 0.35, 0.22, 0.18];
+
+julia> fns = makeminmax([maximum, maximum, maximum, maximum]);
+
+julia> prefs = convert(Array{Function,1}, [prometLinear, prometVShape, prometLinear, prometLinear]);
+
+julia> result = promethee(df, prefs, weights, fns, qs, ps);
+
+julia> result.scores
+8-element Array{Float64,1}:
+  0.0698938775510204
+ -0.148590956382553
+ -0.061361984793917565
+ -0.04780408163265306
+  0.09859591836734694
+ -0.0006389755902360891
+  0.03236974789915966
+  0.057536454581832736
+
+julia> result.bestIndex
+5
+```
+
+# References
+İşletmeciler, Mühendisler ve Yöneticiler için Operasyonel, Yönetsel ve Stratejik Problemlerin
+Çözümünde Çok Kriterli Karar verme Yöntemleri, Editörler: Bahadır Fatih Yıldırım ve Emrah Önder,
+Dora, 2. Basım, 2015, ISBN: 978-605-9929-44-8
+"""
 function promethee(decisionMatrix::DataFrame, prefs::Array{Function,1}, weights::Array{Float64,1}, fns::Array{Function,1}, qs::Array, ps::Array)::PrometheeResult
 
     actionCount, criteriaCount = size(decisionMatrix)
@@ -74,6 +169,11 @@ function promethee(decisionMatrix::DataFrame, prefs::Array{Function,1}, weights:
     
     bestIndex = rankings |> last
 
-    return PrometheeResult(decisionMatrix, weights, scores, rankings, bestIndex)
+    return PrometheeResult(
+        decisionMatrix, 
+        weights, 
+        scores, 
+        rankings, 
+        bestIndex)
 
 end
