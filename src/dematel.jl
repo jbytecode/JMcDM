a6e3893d23a0c894705a38239bd742ece3971779
jbytecode <mhsatman@gmail.com>
2021-02-22 11:53:58 +0300

    add documentation for dematel

--- src/dematel.jl
+++ src/dematel.jl
@@ -1,4 +1,56 @@
-function dematel(comparisonMat::DataFrame; threshold=nothing)
+"""
+        dematel(comparisonMat; threshold = nothing)
+
+Apply Dematel for a given comparison matrix.
+
+
+# Arguments:
+ - `comparisonMat::Array{Float,2}`: n × m matrix of input values. 
+ - `threshold::Union{Nothing, Float64}`: Optional threshold used in calculating values of influence matrix. It is calculated when the argument is omitted. 
+
+# Description 
+dematel() applies the Dematel method to calculate criteria weights, possibly for use in another multi-criteria decision making tool.
+
+# Output 
+- `::DematelResult`: DematelResult object that holds many results including weights for each single criterion.
+
+# Examples
+```julia-repl
+julia> K = [
+        0 3 0 2 0 0 0 0 3 0;
+        3 0 0 0 0 0 0 0 0 2;
+        4 1 0 2 1 3 1 2 3 2;
+        4 1 4 0 1 2 0 1 0 0;
+        3 2 3 1 0 3 0 2 0 0;
+        4 1 4 4 0 0 0 1 1 3;
+        3 0 0 0 0 2 0 0 0 0;
+        3 0 4 3 2 3 1 0 0 0;
+        4 3 2 0 0 1 0 0 0 2;
+        2 1 0 0 0 0 0 0 3 0
+    ];
+
+julia> dmat = makeDecisionMatrix(K);
+julia> result = dematel(dmat);
+
+julia> result.weights
+10-element Array{Float64,1}:
+ 0.1686568559124561
+ 0.07991375718719543
+ 0.14006200243438863
+ 0.10748052790517183
+ 0.08789022388276985
+ 0.12526272598854982
+ 0.03067915023486491
+ 0.10489168834828348
+ 0.092654758940811
+ 0.06250830916550884
+```
+
+# References
+Celikbilek Yakup, Cok Kriterli Karar Verme Yontemleri, Aciklamali ve Karsilastirmali
+Saglik Bilimleri Uygulamalari ile. Editor: Muhlis Ozdemir, Nobel Kitabevi, Ankara, 2018
+"""
+function dematel(comparisonMat::DataFrame; threshold::Union{Nothing,Float64}=nothing)::DematelResult
 
     n, _ = size(comparisonMat)
 
@@ -27,7 +79,7 @@ function dematel(comparisonMat::DataFrame; threshold=nothing)
         for j in 1:n
             if T[i, j] > threshold
                 influence_matrix[i, j] = 1
-            end
+    end
         end
     end
 
