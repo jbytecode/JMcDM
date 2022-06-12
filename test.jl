153b407250276b322ca011a07024e767d5888711
jbytecode <mhsatman@gmail.com>
2022-06-12 21:56:29 +0300

    pump new release of v0.5.0

--- test.jl
+++ test.jl
@@ -1,24 +1,19 @@
 using JMcDM
 
 df = [
-    100.0 200 300 400;
-    1 2 3 4;
-    2 4 6 8;
-    5 7 8 2;
+    GreyNumber(1, 2) GreyNumber(5, 8) GreyNumber(0, 3);
+    GreyNumber(4, 5) GreyNumber(6, 7) GreyNumber(9, 10);
+    GreyNumber(8, 9) GreyNumber(3, 4) GreyNumber(6, 7);
 ]
-        functionlist = makeminmax([maximum, maximum, maximum, maximum])
 
-        w = [0.25, 0.25, 0.25, 0.25] 
+w = [0.50, 0.25, 0.25]
+fns = makeminmax([maximum, maximum, maximum])
+result = topsis(makeDecisionMatrix(df), w, fns)
+@info result 
 
-    row, col = size(df)
-    greydec = Array{GreyNumber, 2}(undef, row, col)
-    for i in 1:row 
-        for j in 1:col 
-            greydec[i, j] = GreyNumber(Float64(df[i, j]))
-        end
-    end
-
-
-    greydf = makeDecisionMatrix(greydec)
-    println(greydf)
-    result = electre(greydf, w,  functionlist)
+# ┌ Info: Scores:
+# │ Any[GreyNumber(0.13192236905307952, 0.4891557063006889), 
+#       GreyNumber(0.2003555029364691, 1.2935321688223387), 
+#       GreyNumber(0.39986397310033467, 1.20680568791548)]
+# │ Best indices:
+# └ 3
